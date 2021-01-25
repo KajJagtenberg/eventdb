5a7c030a96ab2ac392aa39cddfce28de96097e46
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-01-25 17:32:45 +0000

    Streams table

--- webui/pages/index.tsx
+++ webui/pages/index.tsx
@@ -1,33 +1,108 @@
+import Link from 'next/link';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 
-import { Spinner, useToast } from '@chakra-ui/react';
+import {
+    Box, Button, Flex, Link as UILink, Spinner, Table, TableCaption, Tbody, Td, Text, Tfoot, Th,
+    Thead, Tr
+} from '@chakra-ui/react';
 
 import Layout from '../components/Layout';
 import { backend } from '../vars/backend';
 
-const fetchHome = async () => {
-  const response = await fetch(`${backend}/api/v1`);
+const fetchStreams = async (page: number, limit: number) => {
+  const response = await fetch(
+    `${backend}/api/v1/streams?offset=${(page - 1) * limit}&limit=${limit}`
+  );
   return response.json();
 };
 
 const Home = () => {
-  const { data, isLoading } = useQuery('home', fetchHome, {
-    onError: (error: Error) => {
-      toast({
-        title: 'Oops',
-        description: error.message,
-        status: 'error',
-        isClosable: true,
-      });
-    },
-  });
-
-  const toast = useToast();
+  const [page, setPage] = useState(1);
+  const [limit] = useState(10);
+  const { data } = useQuery(
+    ['streams', page, limit],
+    () => fetchStreams(page, limit),
+    { keepPreviousData: true }
+  );
 
   return (
     <Layout title="EventDB">
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-      {isLoading && <Spinner color="teal.500" />}
+      <Flex
+        direction="column"
+        p={4}
+        m={4}
+        bg="white"
+        style={{
+          boxShadow: '0 0 10px #aaa',
+        }}
+      >
+        <Flex direction="row" alignItems="center" mb={8}>
+          <Button
+            colorScheme="teal"
+            size="sm"
+            mx={1}
+            minW={20}
+            onClick={() => setPage(1)}
+            disabled={page === 1}
+          >
+            First
+          </Button>
+
+          <Button
+            colorScheme="teal"
+            size="sm"
+            mx={1}
+            minW={20}
+            onClick={() => setPage((old) => Math.max(1, old - 1))}
+            disabled={page === 1}
+          >
+            Previous
+          </Button>
+
+          <Text fontWeight="semibold" mx={1}>
+            {page}
+          </Text>
+
+          <Button
+            colorScheme="teal"
+            size="sm"
+            mx={1}
+            minW={20}
+            onClick={() => setPage((old) => old + 1)}
+          >
+            Next
+          </Button>
+        </Flex>
+
+        <Table variant="striped" size="sm">
+          <Thead>
+            <Tr>
+              <Th>Row</Th>
+              <Th>Stream</Th>
+            </Tr>
+          </Thead>
+
+          <Tbody>
+            {data &&
+              data.map((stream: string, index: number) => {
+                return (
+                  <Tr key={index}>
+                    <Td>{index + 1}</Td>
+                    <Td>
+                      <Link href={`${backend}/streams/${stream}`}>
+                        <UILink textDecoration="underline" color="blue.600">
+                          {stream}
+                        </UILink>
+                      </Link>
+                    </Td>
+                  </Tr>
+                );
+              })}
+            {!data && <Spinner color="teal.500" />}
+          </Tbody>
+        </Table>
+      </Flex>
     </Layout>
   );
 };
