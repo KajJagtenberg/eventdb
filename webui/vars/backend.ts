f0b506164b062ef7f930fc9c08556927aba4c4da
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-01-27 12:46:50 +0000

    Working Dockerfile

--- webui/vars/backend.ts
+++ webui/vars/backend.ts
@@ -1,2 +1,4 @@
-export const backend =
-  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:6543';
+// export const backend =
+//   process.env.NODE_ENV === 'production' ? '' : 'http://localhost:6543';
+
+export const backend = 'http://localhost:6543';
