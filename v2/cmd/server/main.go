c6346b56403053db2a78214271c3b2350925953d
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-03-19 19:58:47 +0000

    Bare bones gRPC server

---
+++ v2/cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	log.Println("Hello, world!")
+
+	addr := ":6543"
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Unable to listen on %s", addr)
+	}
+	defer lis.Close()
+
+	srv := grpc.NewServer()
+
+	log.Printf("Starting gRPC server on %s", addr)
+
+	if err := srv.Serve(lis); err != nil {
+		log.Fatalf("Unable to start gRPC server: %v", err)
+	}
+}
---
+++ v2/go.mod
@@ -0,0 +1,5 @@
+module github.com/kajjagtenberg/eventflowdb
+
+go 1.16
+
+require google.golang.org/grpc v1.36.0
