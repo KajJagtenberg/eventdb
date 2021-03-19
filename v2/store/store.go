50115e0a96cdb2dba0e2b71a759bfcaeb73f6a3b
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-03-19 21:59:33 +0000

    Added bare bones Storage

--- v2/store/store.go
+++ v2/store/store.go
@@ -15,6 +15,18 @@ type Storage struct {
 	db *bbolt.DB
 }
 
+func (s *Storage) Add(req *AddRequest) error {
+	return nil
+}
+
+func (s *Storage) Get(req *GetRequest) error {
+	return nil
+}
+
+func (s *Storage) Log(req *LogRequest) error {
+	return nil
+}
+
 func NewStorage(db *bbolt.DB) *Storage {
 	return &Storage{db}
 }
