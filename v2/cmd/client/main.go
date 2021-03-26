3e4ac7e8bee699f2df43edb4bd246d772528b922
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-03-26 17:22:52 +0000

    Removed more code and tidied deps

--- v2/cmd/client/main.go
+++ v2/cmd/client/main.go
@@ -1,58 +1,5 @@
 package main
 
-import (
-	"context"
-	"encoding/json"
-	"io"
-	"log"
-	"os"
-	"time"
-
-	"github.com/google/uuid"
-	"github.com/kajjagtenberg/eventflowdb/store"
-	"google.golang.org/grpc"
-)
-
 func main() {
-	enc := json.NewEncoder(os.Stdout)
-	enc.SetIndent("", "  ")
 
-	conn, err := grpc.Dial(":6543", grpc.WithInsecure(), grpc.WithTimeout(time.Second*10))
-	if err != nil {
-		log.Fatalf("Failed to connect: %v", err)
-	}
-	defer conn.Close()
-
-	ctx := context.Background()
-
-	c := store.NewStreamsClient(conn)
-
-	stream := uuid.New()
-
-	result, err := c.Add(ctx, &store.AddRequest{
-		Stream:  stream[:],
-		Version: 0,
-		Events: []*store.AddRequest_Event{
-			{
-				Type:     "TestEvent",
-				Data:     []byte("abcdefghijklmnopqrstuvw"),
-				Metadata: []byte("metadata"),
-			},
-		},
-	})
-	if err != nil {
-		log.Fatalf("Failed to add: %v", err)
-	}
-
-	for {
-		m, err := result.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Failed to read message: %v", err)
-		}
-
-		log.Println(m.String())
-	}
 }
--- v2/go.mod
+++ v2/go.mod
@@ -3,23 +3,15 @@ module github.com/kajjagtenberg/eventflowdb
 go 1.16
 
 require (
-	github.com/99designs/gqlgen v0.13.0
-	github.com/gofiber/adaptor/v2 v2.1.1
 	github.com/gofiber/fiber/v2 v2.6.0
 	github.com/gofiber/helmet/v2 v2.1.0
 	github.com/golang/protobuf v1.5.1
 	github.com/google/uuid v1.2.0
+	github.com/hashicorp/go-uuid v1.0.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/hashicorp/raft v1.2.0
 	github.com/hashicorp/raft-boltdb v0.0.0-20191021154308-4207f1bf0617
-	github.com/joho/godotenv v1.3.0
-	github.com/oklog/ulid v1.3.1
-	github.com/prometheus/client_golang v1.10.0
-	github.com/stretchr/testify v1.7.0
-	github.com/vektah/gqlparser/v2 v2.1.0
+	github.com/stretchr/testify v1.7.0 // indirect
 	go.etcd.io/bbolt v1.3.5
-	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
 	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
-	golang.org/x/text v0.3.5 // indirect
-	google.golang.org/genproto v0.0.0-20210319143718-93e7006c17a6 // indirect
-	google.golang.org/grpc v1.36.0
 )
