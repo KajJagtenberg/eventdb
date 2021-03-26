eb584b1f18eacfda96b7599941d3cd435e2f4ba6
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-03-26 17:20:34 +0000

    Removed dead code

--- v2/cluster/fsm.go
+++ v2/cluster/fsm.go
@@ -3,36 +3,30 @@ package cluster
 import (
 	"io"
 	"log"
-	"strings"
-	"sync"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/hashicorp/raft"
+	"go.etcd.io/bbolt"
 )
 
 type FSM struct {
-	state map[string]string
-	lock  sync.RWMutex
+	db *bbolt.DB
 }
 
 func (fsm *FSM) Apply(applyLog *raft.Log) interface{} {
 	switch applyLog.Type {
 	case raft.LogCommand:
+		var cmd ApplyLog
 
-		cmd := string(applyLog.Data)
-
-		tokens := strings.Split(cmd, " ")
-
-		switch tokens[0] {
-		case "SET":
-			subtokens := strings.Split(tokens[1], "=")
-			key := subtokens[0]
-			value := subtokens[1]
-
-			fsm.Set(key, value)
-		default:
-			log.Println("Unknown command")
+		if err := proto.Unmarshal(applyLog.Data, &cmd); err != nil {
+			log.Printf("Failed to unmarshal ApplyLog: %v", err)
+			return err
 		}
 
+		switch cmd := cmd.Command.(type) {
+		case *ApplyLog_Add:
+			log.Printf("[FSM] Add command received: %v", cmd.Add)
+		}
 	default:
 		log.Println("Type:", applyLog.Type)
 	}
@@ -44,31 +38,25 @@ func (fsm *FSM) Snapshot() (raft.FSMSnapshot, error) {
 	return nil, nil
 }
 
-func (fsm *FSM) Set(key string, value string) {
-	fsm.lock.Lock()
-	defer fsm.lock.Unlock()
-
-	fsm.state[key] = value
-}
-
-func (fsm *FSM) Get(key string) string {
-	fsm.lock.Lock()
-	defer fsm.lock.Unlock()
-
-	return fsm.state[key]
-}
-
-func (fsm *FSM) GetState() map[string]string {
-	fsm.lock.Lock()
-	defer fsm.lock.Unlock()
-
-	return fsm.state
-}
-
 func (fsm *FSM) Restore(io.ReadCloser) error {
 	return nil
 }
 
-func NewFSM() (*FSM, error) {
-	return &FSM{state: make(map[string]string)}, nil
+func NewFSM(db *bbolt.DB) (*FSM, error) {
+	err := db.Update(func(t *bbolt.Tx) error {
+		buckets := []string{"streams", "events"}
+
+		for _, bucket := range buckets {
+			if _, err := t.CreateBucketIfNotExists([]byte(bucket)); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &FSM{db}, nil
 }
--- v2/cmd/server/main.go
+++ v2/cmd/server/main.go
@@ -1,25 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/helmet/v2"
+	"github.com/golang/protobuf/proto"
+	"github.com/google/uuid"
 	"github.com/kajjagtenberg/eventflowdb/cluster"
 	"github.com/kajjagtenberg/eventflowdb/env"
+	"go.etcd.io/bbolt"
 )
 
 var (
-	localID   = env.GetEnv("RAFT_LOCAL_ID", "")
-	bindAddr  = env.GetEnv("RAFT_BIND_ADDR", ":6542")
-	advrAddr  = env.GetEnv("RAFT_ADVR_ADDR", bindAddr)
-	bootstrap = env.GetEnv("RAFT_BOOTSTRAP", "false") == "true"
+	localID       = env.GetEnv("RAFT_LOCAL_ID", "")
+	bindAddr      = env.GetEnv("RAFT_BIND_ADDR", ":6542")
+	advrAddr      = env.GetEnv("RAFT_ADVR_ADDR", bindAddr)
+	bootstrap     = env.GetEnv("RAFT_BOOTSTRAP", "false") == "true"
+	stateLocation = env.GetEnv("STATE_LOCATION", "data/state.dat")
 )
 
 func main() {
-	fsm, err := cluster.NewFSM()
+	db, err := bbolt.Open(stateLocation, 0666, bbolt.DefaultOptions)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	fsm, err := cluster.NewFSM(db)
 	if err != nil {
 		log.Fatalf("Failed to create FSM: %v", err)
 	}
@@ -36,17 +47,41 @@ func main() {
 		},
 	)
 	server.Use(helmet.New())
-	server.Post("/set", func(c *fiber.Ctx) error {
+	server.Post("/add", func(c *fiber.Ctx) error {
 		var body struct {
-			Key   string `json:"key"`
-			Value string `json:"value"`
+			Stream  uuid.UUID                   `json:"stream"`
+			Version uint32                      `json:"version"`
+			Events  []*cluster.AddCommand_Event `json:"events"`
 		}
 
 		if err := c.BodyParser(&body); err != nil {
 			return err
 		}
 
-		cmd := []byte(fmt.Sprintf("SET %s=%s", body.Key, body.Value))
+		if len(body.Events) == 0 {
+			return errors.New("Events cannot be empty")
+		}
+
+		for i, event := range body.Events {
+			if len(event.Type) == 0 {
+				return fmt.Errorf("Type of event at index %d cannot be empty", i)
+			}
+
+			// Validate other fields
+		}
+
+		cmd, err := proto.Marshal(&cluster.ApplyLog{
+			Command: &cluster.ApplyLog_Add{
+				Add: &cluster.AddCommand{
+					Stream:  body.Stream[:],
+					Version: body.Version,
+					Events:  body.Events,
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
 
 		future := raft.Apply(cmd, time.Second*5)
 
@@ -55,9 +90,6 @@ func main() {
 		}
 
 		return c.SendString("Set")
-	})
-	server.Get("/state", func(c *fiber.Ctx) error {
-		return c.JSON(fsm.GetState())
 	})
 
 	if err := server.Listen(":3000"); err != nil {
