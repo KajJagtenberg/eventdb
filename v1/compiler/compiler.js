f414420e2f056d5fb2a0bf87fe259054627c4065
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-03-28 18:34:54 +0000

    Moved v2 to root

--- v1/compiler/compiler.js
+++
@@ -1,5 +0,0 @@
-var compile = function compile(src) {
-  return Babel.transform(src, {
-    presets: ["es2016"],
-  }).code;
-};
--- v1/constants/constants.go
+++
@@ -1,6 +0,0 @@
-package constants
-
-const (
-	Name    = "EventflowDB"
-	Version = "0.2.0"
-)
