d97acad73bc04b7f2f16310b391d6680bc5cfab5
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-03-29 15:10:21 +0000

    Updated cluster tab

--- webui/components/ClusterTab.tsx
+++ webui/components/ClusterTab.tsx
@@ -2,13 +2,19 @@ import { gql, useQuery } from '@apollo/client';
 import { Flex, Text } from '@chakra-ui/react';
 
 export const ClusterTab = () => {
-  const { data } = useQuery(gql`
-    {
-      cluster {
-        leader
+  const { data } = useQuery(
+    gql`
+      {
+        cluster {
+          leader
+          size
+        }
       }
+    `,
+    {
+      pollInterval: 1000,
     }
-  `);
+  );
 
   return (
     <>
@@ -20,6 +26,16 @@ export const ClusterTab = () => {
         </Flex>
 
         <Flex flexDirection="column">{data?.cluster.leader}</Flex>
+      </Flex>
+
+      <Flex>
+        <Flex>
+          <Text fontWeight="semibold" mr={2} color="brand.500">
+            Size:
+          </Text>
+        </Flex>
+
+        <Flex flexDirection="column">{data?.cluster.size}</Flex>
       </Flex>
     </>
   );
