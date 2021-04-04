365a09f2a5ffb1f573f2dce7370a02cd1240323e
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-04-04 14:37:35 +0000

    Implemented Add on BoltStore with tests

--- store/bolt.go
+++ store/bolt.go
@@ -1,13 +1,20 @@
 package store
 
 import (
+	"bytes"
+	"errors"
 	"io"
+	"math/rand"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/oklog/ulid"
 	"go.etcd.io/bbolt"
 )
 
 var (
 	buckets = []string{"streams", "events"}
+	entropy = ulid.Monotonic(rand.New(rand.NewSource(int64(ulid.Now()))), 0)
 )
 
 type BoltStore struct {
@@ -34,6 +41,91 @@ func (s *BoltStore) Backup(dst io.Writer) error {
 
 		return nil
 	})
+}
+
+func (s *BoltStore) Add(stream uuid.UUID, version uint32, events []EventData) ([]Event, error) {
+	var result []Event
+
+	if err := s.db.Update(func(t *bbolt.Tx) error {
+		streamBucket := t.Bucket([]byte("streams"))
+		eventsBucket := t.Bucket([]byte("streams"))
+
+		var s Stream
+
+		if value := streamBucket.Get(stream[:]); value != nil {
+			if err := s.Unmarshal(value); err != nil {
+				return err
+			}
+		} else {
+			s.ID = stream
+			s.AddedAt = time.Now()
+		}
+
+		if len(s.Events) != int(version) {
+			return errors.New("Concurrent stream modification")
+		}
+
+		now := time.Now()
+
+		for i, event := range events {
+			id, err := ulid.New(ulid.Now(), entropy)
+			if err != nil {
+				return err
+			}
+
+			record := Event{
+				ID:            id,
+				Stream:        stream,
+				Version:       version + uint32(i),
+				Type:          event.Type,
+				Data:          event.Data,
+				Metadata:      event.Metadata,
+				CausationID:   event.CausationID,
+				CorrelationID: event.CorrelationID,
+				AddedAt:       event.AddedAt,
+			}
+
+			if bytes.Compare(record.CausationID[:], make([]byte, 16)) == 0 {
+				record.CausationID = record.ID
+			}
+
+			if bytes.Compare(record.CorrelationID[:], make([]byte, 16)) == 0 {
+				record.CorrelationID = record.CausationID
+			}
+
+			if record.AddedAt.IsZero() {
+				record.AddedAt = now
+			}
+
+			result = append(result, record)
+
+			s.Events = append(s.Events, record.ID)
+		}
+
+		for _, record := range result {
+			if value, err := record.Marshal(); err != nil {
+				return err
+			} else {
+				if err := eventsBucket.Put(record.ID[:], value); err != nil {
+					return err
+				}
+			}
+		}
+
+		if value, err := s.Marshal(); err != nil {
+			return err
+		} else {
+			if err := streamBucket.Put(s.ID[:], value); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
 
 func NewBoltStore(db *bbolt.DB) (*BoltStore, error) {
---
+++ store/bolt_test.go
@@ -0,0 +1,87 @@
+package store
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"go.etcd.io/bbolt"
+)
+
+func TestBoltAdd(t *testing.T) {
+	assert := assert.New(t)
+
+	f, err := ioutil.TempFile("/tmp", "*")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	db, err := bbolt.Open(f.Name(), 0666, bbolt.DefaultOptions)
+	if err != nil {
+		t.Fatal(err)
+	}
+	store, err := NewBoltStore(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stream := uuid.New()
+	events := []EventData{
+		{
+			Type:     "TestEvent",
+			Data:     []byte("data"),
+			Metadata: []byte("metadata"),
+		},
+	}
+
+	records, err := store.Add(stream, 0, events)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(len(records), len(events))
+
+	for i := 0; i < len(events); i++ {
+		assert.Equal(events[i].Type, records[i].Type)
+		assert.Equal(events[i].Data, records[i].Data)
+		assert.Equal(events[i].Metadata, records[i].Metadata)
+		assert.NotEqual(events[i].CausationID, records[i].CausationID)
+		assert.NotEqual(events[i].CorrelationID, records[i].CorrelationID)
+	}
+}
+
+func TestBoltAddConcurrency(t *testing.T) {
+	assert := assert.New(t)
+
+	f, err := ioutil.TempFile("/tmp", "*")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	db, err := bbolt.Open(f.Name(), 0666, bbolt.DefaultOptions)
+	if err != nil {
+		t.Fatal(err)
+	}
+	store, err := NewBoltStore(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stream := uuid.New()
+	events := []EventData{
+		{
+			Type:     "TestEvent",
+			Data:     []byte("data"),
+			Metadata: []byte("metadata"),
+		},
+	}
+
+	_, err = store.Add(stream, 0, events)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = store.Add(stream, 0, events)
+	assert.NotEqual(err, nil)
+}
