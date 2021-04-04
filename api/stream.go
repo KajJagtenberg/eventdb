785b623f20ce61f89c855dccb0a75a4ee4f443f3
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-04-04 15:35:34 +0000

    Implemented AddEvents

--- api/stream.go
+++ api/stream.go
@@ -2,16 +2,75 @@ package api
 
 import (
 	context "context"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/kajjagtenberg/eventflowdb/store"
+	"github.com/oklog/ulid"
 )
 
 type StreamService struct {
 	store store.Store
 }
 
-func (service *StreamService) AddEvents(context.Context, *AddEventsRequest) (*AddEventsResponse, error) {
-	return nil, nil
+func (service *StreamService) AddEvents(ctx context.Context, req *AddEventsRequest) (*AddEventsResponse, error) {
+	var stream uuid.UUID
+	if err := stream.UnmarshalBinary(req.Stream); err != nil {
+		return nil, err
+	}
+
+	version := req.Version
+
+	var events []store.EventData
+
+	for _, event := range req.Events {
+		var causationID ulid.ULID
+		var correlationID ulid.ULID
+
+		if id := event.CausationId; id != nil {
+			if err := causationID.UnmarshalBinary(id); err != nil {
+				return nil, err
+			}
+		}
+
+		if id := event.CorrelationId; id != nil {
+			if err := correlationID.UnmarshalBinary(id); err != nil {
+				return nil, err
+			}
+		}
+
+		events = append(events, store.EventData{
+			Type:          event.Type,
+			Data:          event.Data,
+			Metadata:      event.Metadata,
+			CausationID:   causationID,
+			CorrelationID: correlationID,
+			AddedAt:       time.Unix(0, event.AddedAt),
+		})
+	}
+
+	records, err := service.store.Add(stream, version, events)
+	if err != nil {
+		return nil, err
+	}
+
+	result := &AddEventsResponse{}
+
+	for _, record := range records {
+		result.Events = append(result.Events, &Event{
+			Id:            record.ID[:],
+			Stream:        record.Stream[:],
+			Version:       record.Version,
+			Type:          record.Type,
+			Data:          record.Data,
+			Metadata:      record.Metadata,
+			CausationId:   record.CausationID[:],
+			CorrelationId: record.CorrelationID[:],
+			AddedAt:       record.AddedAt.UnixNano(),
+		})
+	}
+
+	return result, nil
 }
 
 func (service *StreamService) GetEvents(context.Context, *GetEventsRequest) (*GetEventsResponse, error) {
