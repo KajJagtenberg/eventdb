8a78a45b74d7c4c1245e444819752c89a7d338b1
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-04-24 12:25:50 +0000

    Implemented more api functions

--- api/api.go
+++ api/api.go
@@ -1,11 +1,14 @@
 package api
 
 import (
+	"encoding/json"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/kajjagtenberg/eventflowdb/constants"
 	"github.com/kajjagtenberg/eventflowdb/store"
+	"github.com/oklog/ulid"
 	"github.com/tidwall/redcon"
 )
 
@@ -83,6 +86,55 @@ func (r *Resp) CommandHandler(conn redcon.Conn, cmd redcon.Command) {
 		}
 
 		conn.WriteInt64(count)
+	case "log":
+		var offset ulid.ULID
+		var limit int
+		var err error
+
+		switch len(cmd.Args) {
+		case 1:
+			offset = ulid.ULID{}
+			limit = 10
+		case 2:
+			offset, err = ulid.Parse(string(cmd.Args[1]))
+			if err != nil {
+				conn.WriteError(err.Error())
+				return
+			}
+		case 3:
+			offset, err = ulid.Parse(string(cmd.Args[1]))
+			if err != nil {
+				conn.WriteError(err.Error())
+				return
+			}
+
+			limit, err = strconv.Atoi(string(cmd.Args[2]))
+			if err != nil {
+				conn.WriteError(err.Error())
+				return
+			}
+		default:
+			conn.WriteError("Amount of arguments is not supported")
+			return
+		}
+
+		events, err := r.store.Log(offset, uint32(limit))
+		if err != nil {
+			conn.WriteError(err.Error())
+			return
+		}
+
+		conn.WriteArray(len(events))
+
+		for _, event := range events {
+			raw, err := json.Marshal(event)
+			if err != nil {
+				conn.WriteError(err.Error())
+				return
+			}
+
+			conn.WriteString(string(raw))
+		}
 	}
 }
 
@@ -90,9 +142,7 @@ func (r *Resp) AcceptHandler(conn redcon.Conn) bool {
 	return true
 }
 
-func (r *Resp) ErrorHandler(conn redcon.Conn, err error) {
-
-}
+func (r *Resp) ErrorHandler(conn redcon.Conn, err error) {}
 
 func NewResp(store store.Store) *Resp {
 	return &Resp{store}
