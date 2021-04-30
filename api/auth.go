9552f6f03825bae66f11af942dc3fdaac014160b
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-04-30 10:08:38 +0000

    Updated authentication

--- api/auth.go
+++ api/auth.go
@@ -8,33 +8,39 @@ import (
 
 func Authentication(password string) Handler {
 	return func(conn redcon.Conn, cmd redcon.Command) bool {
-		if password == "" {
-			return true
-		}
-
 		session := conn.Context().(*Session)
-
-		if session.Authenticated {
-			return true
-		}
 
 		query := strings.ToLower(string(cmd.Args[0]))
 
-		if query != "auth" {
+		if session.Authenticated {
+			if query == "auth" {
+				conn.WriteError("Already authenticated")
+				return false
+			} else {
+				return true
+			}
+		} else {
+			if password == "" {
+				return true
+			}
+
+			if query != "auth" {
+				conn.WriteError("Unauthorized")
+				return false
+			}
+
+			if string(cmd.Args[1]) != password {
+				conn.WriteError("Unauthorized")
+				return false
+			}
+
+			session.Authenticated = true
+
+			conn.SetContext(session)
+
+			conn.WriteString("OK")
+
 			return false
 		}
-
-		if string(cmd.Args[1]) != password {
-			conn.WriteError("Unauthorized")
-			return false
-		}
-
-		session.Authenticated = true
-
-		conn.SetContext(session)
-
-		conn.WriteString("OK")
-
-		return false
 	}
 }
