3f5a50bf5dc91e6404b03bcf8a819c0b3a6c2bf1
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-05-01 13:31:53 +0000

    Added Version command to client

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,9 +3,7 @@ package main
 import (
 	"log"
 
-	"github.com/google/uuid"
 	v1 "github.com/kajjagtenberg/eventflowdb/client"
-	"github.com/kajjagtenberg/eventflowdb/store"
 )
 
 func main() {
@@ -13,22 +11,5 @@ func main() {
 		Address: "127.0.0.1:6543",
 	})
 
-	stream := uuid.New()
-
-	_, err := client.Add(stream, 0, []store.EventData{
-		{
-			Type: "TestEvent",
-			Data: []byte("This is the data"),
-		},
-	})
-	if err != nil {
-		log.Fatalf("Failed to add events: %v", err)
-	}
-
-	events, err := client.GetAll(v1.SINCE_START, 10)
-	if err != nil {
-		log.Fatalf("Failed to get events: %v", err)
-	}
-
-	log.Println(events)
+	log.Println(client.Version())
 }
