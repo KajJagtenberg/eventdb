41836c95212455f40aa2c3506de4a90bf10e7c31
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-05-14 16:58:18 +0000

    Updated CommandHandler to a function

--- commands/command.go
+++ commands/command.go
@@ -1,13 +1,17 @@
 package commands
 
+import "errors"
+
+var (
+	ErrUnknownCommand = errors.New("unknown command")
+)
+
 type Command struct {
 	Name string `json:"name"`
 	Args []byte `json:"args"`
 }
 
-type CommandHandler interface {
-	Handle(cmd Command) (interface{}, error)
-}
+type CommandHandler = func(cmd Command) (interface{}, error)
 
 type CommandRegistry struct {
 	handlers map[string]CommandHandler
@@ -16,6 +20,15 @@ type CommandRegistry struct {
 func (r *CommandRegistry) Register(name string, shorthand string, handler CommandHandler) {
 	r.handlers[name] = handler
 	r.handlers[shorthand] = handler
+}
+
+func (r *CommandRegistry) Handle(cmd Command) (interface{}, error) {
+	handler := r.handlers[cmd.Name]
+	if handler == nil {
+		return nil, ErrUnknownCommand
+	}
+
+	return handler(cmd)
 }
 
 func NewCommandRegistry() *CommandRegistry {
