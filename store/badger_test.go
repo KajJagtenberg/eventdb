d53b59072dc1bba95aa465a3f89b71f9b7fe01a0
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-05-24 11:54:28 +0000

    Added test

--- store/badger_test.go
+++ store/badger_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/dgraph-io/badger/v3"
 	"github.com/google/uuid"
 	"github.com/kajjagtenberg/eventflowdb/store"
+	"github.com/oklog/ulid"
+	"github.com/stretchr/testify/assert"
 )
 
 func BenchmarkAddMemory(t *testing.B) {
@@ -44,4 +46,50 @@ func BenchmarkAddMemory(t *testing.B) {
 			}
 		}
 	}
+}
+
+func TestAdd(t *testing.T) {
+	db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true).WithLogger(nil))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	eventstore, err := store.NewBadgerEventStore(store.BadgerStoreOptions{
+		DB:             db,
+		EstimateCounts: false,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stream := uuid.New()
+
+	data := make([]byte, 100)
+	metadata := make([]byte, 50)
+	rand.Read(data)
+	rand.Read(metadata)
+
+	events, err := eventstore.Add(stream, 0, []store.EventData{
+		{
+			Type:     "AccountOpened",
+			Data:     data,
+			Metadata: metadata,
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert := assert.New(t)
+	assert.Equal(len(events), 1)
+	assert.Equal(data, events[0].Data)
+	assert.Equal(metadata, events[0].Metadata)
+	assert.Equal(events[0].Stream, stream)
+	assert.Equal(events[0].Type, "AccountOpened")
+	assert.Equal(events[0].AddedAt.IsZero(), false)
+	assert.NotEqual(events[0].ID, ulid.ULID{})
+	assert.Equal(events[0].ID, events[0].CausationID)
+	assert.Equal(events[0].ID, events[0].CorrelationID)
+	assert.Equal(events[0].Version, 0)
 }
