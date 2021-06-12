f0d464152743d511ee891dc367d3d6dc608b86d3
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-06-12 20:24:46 +0000

    Added commands to FSM

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -37,6 +37,110 @@ func (b *fsm) Apply(log *raft.Log) interface{} {
 				Data:  res,
 				Error: err,
 			}
+
+		case "GET":
+			var req *api.GetRequest
+			if err := proto.Unmarshal(cmd.Payload, req); err != nil {
+				return &ApplyResponse{
+					Error: err,
+				}
+			}
+			res, err := b.store.Get(req)
+			return &ApplyResponse{
+				Data:  res,
+				Error: err,
+			}
+
+		case "GET_ALL":
+			var req *api.GetAllRequest
+			if err := proto.Unmarshal(cmd.Payload, req); err != nil {
+				return &ApplyResponse{
+					Error: err,
+				}
+			}
+			res, err := b.store.GetAll(req)
+			return &ApplyResponse{
+				Data:  res,
+				Error: err,
+			}
+
+		case "EVENT_COUNT":
+			var req *api.EventCountRequest
+			if err := proto.Unmarshal(cmd.Payload, req); err != nil {
+				return &ApplyResponse{
+					Error: err,
+				}
+			}
+			res, err := b.store.EventCount(req)
+			return &ApplyResponse{
+				Data:  res,
+				Error: err,
+			}
+
+		case "EVENT_COUNT_ESTIMATE":
+			var req *api.EventCountEstimateRequest
+			if err := proto.Unmarshal(cmd.Payload, req); err != nil {
+				return &ApplyResponse{
+					Error: err,
+				}
+			}
+			res, err := b.store.EventCountEstimate(req)
+			return &ApplyResponse{
+				Data:  res,
+				Error: err,
+			}
+
+		case "STREAM_COUNT":
+			var req *api.StreamCountRequest
+			if err := proto.Unmarshal(cmd.Payload, req); err != nil {
+				return &ApplyResponse{
+					Error: err,
+				}
+			}
+			res, err := b.store.StreamCount(req)
+			return &ApplyResponse{
+				Data:  res,
+				Error: err,
+			}
+
+		case "STREAM_COUNT_ESTIMATE":
+			var req *api.StreamCountEstimateRequest
+			if err := proto.Unmarshal(cmd.Payload, req); err != nil {
+				return &ApplyResponse{
+					Error: err,
+				}
+			}
+			res, err := b.store.StreamCountEstimate(req)
+			return &ApplyResponse{
+				Data:  res,
+				Error: err,
+			}
+
+		case "LIST_STREAMS":
+			var req *api.ListStreamsRequest
+			if err := proto.Unmarshal(cmd.Payload, req); err != nil {
+				return &ApplyResponse{
+					Error: err,
+				}
+			}
+			res, err := b.store.ListStreams(req)
+			return &ApplyResponse{
+				Data:  res,
+				Error: err,
+			}
+
+		case "SIZE":
+			var req *api.SizeRequest
+			if err := proto.Unmarshal(cmd.Payload, req); err != nil {
+				return &ApplyResponse{
+					Error: err,
+				}
+			}
+			res, err := b.store.Size(req)
+			return &ApplyResponse{
+				Data:  res,
+				Error: err,
+			}
 		}
 	}
 
