1cb0d6d17253046cced5bae7238df6e0d4630bfb
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-06-12 12:23:44 +0000

    Implemented get

--- fsm/badger.go
+++ fsm/badger.go
@@ -60,7 +60,40 @@ func (b *badgerFSM) set(key string, value interface{}) error {
 }
 
 func (b *badgerFSM) get(key string) (interface{}, error) {
-	return nil, nil
+	var keyByte = []byte(key)
+	var data interface{}
+
+	txn := b.db.NewTransaction(false)
+	defer func() {
+		txn.Commit()
+	}()
+
+	item, err := txn.Get(keyByte)
+	if err != nil {
+		data = map[string]interface{}{}
+		return data, err
+	}
+
+	var value = make([]byte, 0)
+	err = item.Value(func(val []byte) error {
+		value = append(value, val...)
+		return nil
+	})
+
+	if err != nil {
+		data = map[string]interface{}{}
+		return data, err
+	}
+
+	if len(value) > 0 {
+		err = json.Unmarshal(value, &data)
+	}
+
+	if err != nil {
+		data = map[string]interface{}{}
+	}
+
+	return data, err
 }
 
 func (b *badgerFSM) delete(key string) error {
