98726390fbccb266185aaba114ebf921d0539575
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-06-12 12:19:47 +0000

    Set up helper functions

--- fsm/badger.go
+++ fsm/badger.go
@@ -1,7 +1,9 @@
 package fsm
 
 import (
+	"encoding/json"
 	"io"
+	"strings"
 
 	"github.com/dgraph-io/badger/v3"
 	"github.com/hashicorp/raft"
@@ -12,6 +14,22 @@ type badgerFSM struct {
 }
 
 func (b *badgerFSM) Apply(log *raft.Log) interface{} {
+	switch log.Type {
+	case raft.LogCommand:
+		var payload = CommandPayload{}
+		if err := json.Unmarshal(log.Data, &payload); err != nil {
+			return nil
+		}
+
+		op := strings.ToUpper(strings.TrimSpace(payload.Operation))
+
+		switch op {
+		case "SET":
+		case "GET":
+		case "DELETE":
+		}
+	}
+
 	return nil
 }
 
@@ -20,6 +38,17 @@ func (b *badgerFSM) Snapshot() (raft.FSMSnapshot, error) {
 }
 
 func (b *badgerFSM) Restore(io.ReadCloser) error {
+	return nil
+}
+func (b *badgerFSM) set(key, value string) error {
+	return nil
+}
+
+func (b *badgerFSM) get(key string) (interface{}, error) {
+	return nil, nil
+}
+
+func (b *badgerFSM) delete(key string) error {
 	return nil
 }
 
