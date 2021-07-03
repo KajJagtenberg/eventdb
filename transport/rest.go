8db7a36e172074aaee5aacc589491a668789ce77
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-07-03 18:56:37 +0000

    Implemented REST routes

--- transport/rest.go
+++ transport/rest.go
@@ -2,12 +2,14 @@ package transport
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/eventflowdb/eventflowdb/api"
 	"github.com/eventflowdb/eventflowdb/constants"
 	"github.com/eventflowdb/eventflowdb/env"
 	"github.com/eventflowdb/eventflowdb/store"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/sirupsen/logrus"
 )
 
@@ -37,55 +39,167 @@ func stream(eventstore store.EventStore, logger *logrus.Logger) fiber.Handler {
 		req.Limit = uint32(limit)
 
 		res, err := eventstore.GetStream(&req)
-		switch err {
-		default:
-			logger.Println(err)
-			return fiber.ErrInternalServerError
+		if err != nil {
+			switch err {
+			default:
+				logger.Println(err)
+				return fiber.ErrInternalServerError
+			}
 		}
 
 		return c.JSON(res)
 	}
 }
 
-func all() fiber.Handler {
+func all(eventstore store.EventStore, logger *logrus.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return fiber.ErrNotImplemented
+		var req api.GetGlobalStreamRequest
+		offset, err := strconv.ParseUint(c.Query("offset", "0"), 10, 64)
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+		limit, err := strconv.ParseUint(c.Query("limit", "0"), 10, 32)
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+
+		req.Offset = offset
+		req.Limit = uint32(limit)
+
+		res, err := eventstore.GetGlobalStream(&req)
+		if err != nil {
+			switch err {
+			default:
+				logger.Println(err)
+				return fiber.ErrInternalServerError
+			}
+		}
+
+		return c.JSON(res)
 	}
 }
 
-func add() fiber.Handler {
+func appendStream(eventstore store.EventStore, logger *logrus.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return fiber.ErrNotImplemented
+		var req api.AppendStreamRequest
+		if err := c.BodyParser(&req); err != nil {
+			return fiber.ErrBadRequest
+		}
+
+		res, err := eventstore.AppendStream(&req)
+		if err != nil {
+			switch err {
+			default:
+				logger.Println(err)
+				return fiber.ErrInternalServerError
+			}
+		}
+
+		return c.JSON(res)
 	}
 }
 
-func streamCount() fiber.Handler {
+func streamCount(eventstore store.EventStore, logger *logrus.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return fiber.ErrNotImplemented
+		var res interface{}
+		var err error
+
+		if c.Query("estimate", "false") == "true" {
+			res, err = eventstore.StreamCountEstimate(&api.StreamCountEstimateRequest{})
+		} else {
+			res, err = eventstore.StreamCount(&api.StreamCountRequest{})
+		}
+
+		if err != nil {
+			switch err {
+			default:
+				logger.Println(err)
+				return fiber.ErrInternalServerError
+			}
+		}
+
+		return c.JSON(res)
 	}
 }
 
-func event() fiber.Handler {
+func event(eventstore store.EventStore, logger *logrus.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return fiber.ErrNotImplemented
+		var req api.GetEventRequest
+		req.Id = c.Params("id")
+
+		res, err := eventstore.GetEvent(&req)
+		if err != nil {
+			switch err {
+			default:
+				logger.Println(err)
+				return fiber.ErrInternalServerError
+			}
+		}
+
+		return c.JSON(res)
 	}
 }
 
-func eventCount() fiber.Handler {
+func eventCount(eventstore store.EventStore, logger *logrus.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return fiber.ErrNotImplemented
+		var res interface{}
+		var err error
+
+		if c.Query("estimate", "false") == "true" {
+			res, err = eventstore.EventCountEstimate(&api.EventCountEstimateRequest{})
+		} else {
+			res, err = eventstore.EventCount(&api.EventCountRequest{})
+		}
+
+		if err != nil {
+			switch err {
+			default:
+				logger.Println(err)
+				return fiber.ErrInternalServerError
+			}
+		}
+
+		return c.JSON(res)
 	}
 }
 
 func uptime() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return fiber.ErrNotImplemented
+		uptime := time.Since(start)
+
+		return c.JSON(api.UptimeResponse{
+			Uptime:      uptime.Milliseconds(),
+			UptimeHuman: uptime.String(),
+		})
 	}
 }
 
-func streams() fiber.Handler {
+func streams(eventstore store.EventStore, logger *logrus.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return fiber.ErrNotImplemented
+		var req api.ListStreamsRequest
+
+		offset, err := strconv.ParseUint(c.Query("offset", "0"), 10, 32)
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+		limit, err := strconv.ParseUint(c.Query("limit", "0"), 10, 32)
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+
+		req.Skip = uint32(offset)
+		req.Limit = uint32(limit)
+
+		res, err := eventstore.ListStreams(&req)
+		if err != nil {
+			switch err {
+			default:
+				logger.Println(err)
+				return fiber.ErrInternalServerError
+			}
+		}
+
+		return c.JSON(res)
 	}
 }
 
@@ -100,15 +214,16 @@ func RunRestServer(eventstore store.EventStore, logger *logrus.Logger) *fiber.Ap
 	})
 
 	v1 := server.Group("/api/v1")
+	v1.Use(compress.New())
 	v1.Get("/version", version())
-	v1.Get("/stream/all", all())
-	v1.Get("/stream/count", streamCount())
+	v1.Get("/stream/all", all(eventstore, logger))
+	v1.Get("/stream/count", streamCount(eventstore, logger))
 	v1.Get("/stream/:id", stream(eventstore, logger))
-	v1.Post("/stream/:id", add())
-	v1.Get("/event/count", eventCount())
-	v1.Get("/event/:id", event())
+	v1.Post("/stream/:id", appendStream(eventstore, logger))
+	v1.Get("/event/count", eventCount(eventstore, logger))
+	v1.Get("/event/:id", event(eventstore, logger))
 	v1.Get("/uptime", uptime())
-	v1.Get("/streams", streams())
+	v1.Get("/streams", streams(eventstore, logger))
 
 	go func() {
 		logger.Printf("REST server listening on %s", httpPort)
